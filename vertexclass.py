2d144294378ad00971b4d97dd8fba4f13d8b0f36
Erin Ellefsen <elleer02@erins-mbp-4.dynamic.luther.edu>
2015-06-03 10:44:29 -0500

    deleted methods in vertex class and created disease class

--- vertexclass.py
+++ vertexclass.py
@@ -1,14 +1,10 @@
 import sys
 
 class Vertex:
-    def __init__(self,key):
+    def __init__(self,key,disease):
         self.id = key
         self.connectedTo = {}
-        self.color = 'white'
-        self.dist = sys.maxsize
-        self.pred = None
-        self.disc = 0
-        self.fin = 0
+        self.status = 'S'
         #self.cost = {}
     def addNeighbor(self,nbr,weight=0):
         self.connectedTo[nbr] = weight
@@ -20,23 +16,7 @@ class Vertex:
         return self.id
     def getWeight(self,nbr):
         return self.connectedTo[nbr]
-    def setColor(self,color):
-        self.color = color
-    def setDistance(self,d):
-        self.dist = d
-    def setPred(self,p):
-        self.pred = p
-    def setDiscovery(self,dtime):
-        self.disc = dtime
-    def setFinish(self,ftime):
-        self.fin = ftime
-    def getFinish(self):
-        return self.fin
-    def getDiscovery(self):
-        return self.disc
-    def getPred(self):
-        return self.pred
-    def getDistance(self):
-        return self.dist
-    def getColor(self):
-        return self.color
+    def setStatus(self,status):
+        self.status = status
+    def getStatus(self):
+        return self.status
