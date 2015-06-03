7b3de13d90bb6d7d0aab53c461790fe39c808223
Erin Ellefsen <elleer02@erins-mbp-4.dynamic.luther.edu>
2015-06-03 16:26:03 -0500

    made new lists of i and r and difference combos

--- graph.py
+++ graph.py
@@ -8,6 +8,9 @@ class Graph:
         self.p = p
         self.r = r
         self.infect = initinfect
+        self.ilist = []
+        self.rlist = []
+        self.iandrlist = []
 
     def getVertices(self):
         return self.vertices
@@ -28,77 +31,47 @@ class Graph:
                         item.addNeighbor(item2)
 
 
-    def update(self):
-        s = 0
-        i = 0
-        r = 0
-        for item in self.vertices:
-            if item.getStatus() == 'S':
-                s += 1
-            if item.getStatus() == 'I':
-                i += 1
-            if item.getStatus() == "R":
-                r += 1
-            item.update()
-        print("S is",s,"I is",i,"R is",r)
+    def update(self, numrepetitions):
+        for stuff in range(0,numrepetitions):
+            s = 0
+            i = 0
+            r = 0
+
+            for item in self.vertices:
+                if item.getStatus() == 'S':
+                   s += 1
+                if item.getStatus() == 'I':
+                    i += 1
+                if item.getStatus() == 'R':
+                    r += 1
+                item.update()
+            self.ilist = self.ilist + [i]
+            self.rlist = self.rlist + [r]
+            self.iandrlist = self.iandrlist + [i+r]
+            #print("S is",s,"I is",i,"R is",r)
+        newlist = []
+        for item in range(len(self.ilist)-1):
+            newlist = newlist + [self.ilist[item + 1] - self.ilist[item]]
+        newlist2 = []
+        for item in range(len(self.rlist) -1):
+            newlist2 = newlist2 + [self.rlist[item+1] - self.rlist[item]]
+        newlist3 = []
+        for item in range(len(self.iandrlist)-1):
+            newlist3 = newlist3 + [self.iandrlist[item+1] - self.iandrlist[item]]
+        print(newlist3,sum(newlist3))
+
+
+
+
 
 def main():
+    for x in range(10):
+    #duration,prob of infection, prob of recov, initial infection
+        g = Graph(3, .02, .1, .01)
+        g.makeVertices(100)         # of people
+        g.makeConnections(.03)         #prob they are connected
+        g.update(40)
 
-     #duration,prob of infection, prob of recov, initial infection
-    g = Graph(3, .02, .05, .1)
-    g.makeVertices(2000)         # of people
-    g.makeConnections(.005)         #prob they are connected
-    for i in range(0,40):
-
-
-
-
-
-
-
-        g.update()
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
