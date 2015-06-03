06e999e9a0a3288c190270dfc37c9b8e4183e23c
JohnDoorenbos <doorjo01@luther.edu>
2015-06-03 12:31:35 -0500

    added update method for vertices and tested the spread of disease in our manually created graph

--- graph.py
+++ graph.py
@@ -1,28 +1,39 @@
 import vertexclass
 import disease
 def main():
-    A = vertexclass.Vertex("A",disease.Disease(3,.45,.1))
-    B = vertexclass.Vertex("B",disease.Disease(3,.45,.1))
-    C = vertexclass.Vertex("C",disease.Disease(3,.45,.1))
-    D = vertexclass.Vertex("D",disease.Disease(3,.45,.1))
-    E = vertexclass.Vertex("E",disease.Disease(3,.45,.1))
-    F = vertexclass.Vertex("F",disease.Disease(3,.45,.1))
-    G = vertexclass.Vertex("G",disease.Disease(3,.45,.1))
-    H = vertexclass.Vertex("H",disease.Disease(3,.45,.1))
-    I = vertexclass.Vertex("I",disease.Disease(3,.45,.1))
-    A.addNeighbor(B)
-    A.addNeighbor(C)
-    B.addNeighbor(D)
-    B.addNeighbor(G)
-    C.addNeighbor(E)
-    C.addNeighbor(F)
-    D.addNeighbor(F)
-    E.addNeighbor(H)
-    F.addNeighbor(H)
-    F.addNeighbor(I)
-    H.addNeighbor(I)
-    A.setStatus('I')
-    C.setStatus('I')
-    E.setStatus('I')
-    print(A.getConnections())
+    count = 0
+    for i in range(5):
+
+        A = vertexclass.Vertex("A",disease.Disease(3,.45,.1))
+        B = vertexclass.Vertex("B",disease.Disease(3,.45,.1))
+        C = vertexclass.Vertex("C",disease.Disease(3,.45,.1))
+        D = vertexclass.Vertex("D",disease.Disease(3,.45,.1))
+        E = vertexclass.Vertex("E",disease.Disease(3,.45,.1))
+        F = vertexclass.Vertex("F",disease.Disease(3,.45,.1))
+        G = vertexclass.Vertex("G",disease.Disease(3,.45,.1))
+        H = vertexclass.Vertex("H",disease.Disease(3,.45,.1))
+        I = vertexclass.Vertex("I",disease.Disease(3,.45,.1))
+        
+        vertLst = [A,B,C,D,E,F,G,H,I]
+        
+        A.addNeighbor(B)
+        A.addNeighbor(C)
+        B.addNeighbor(D)
+        B.addNeighbor(G)
+        C.addNeighbor(E)
+        C.addNeighbor(F)
+        D.addNeighbor(F)
+        E.addNeighbor(H)
+        F.addNeighbor(H)
+        F.addNeighbor(I)
+        H.addNeighbor(I)
+        A.initialInfect()
+        for i in range(5):
+            for i in vertLst:
+                i.update()
+            print(vertLst)
+                
+        count += 1
+        print("End case ",count)
+
 main()
