12753d4cf029bb1c6f3e5459140df909c07f7f2f
JohnDoorenbos <doorjo01@luther.edu>
2015-06-04 11:46:59 -0500

    Modified cleanGraphs.py to have a more modular layout

--- cleanGraph.py
+++ cleanGraph.py
@@ -7,11 +7,19 @@ class Graph:
         self.k = k
         self.p = p
         self.r = r
-        self.infect = initinfect
+        self.infectRate = initinfect
         self.ilist = []
         self.rlist = []
         self.iandrlist = []
-
+        self.numS = 0
+        self.numI = 0
+        self.numR = 0
+        self.highEpi = False
+        self.finalEpi = False
+    
+    def resetGraph(self):
+        '''We need to save the original state of the graph'''
+        pass
     def getVertices(self):
         return self.vertices
 
@@ -31,48 +39,48 @@ class Graph:
                         item.addNeighbor(item2)
 
 
-    def update(self, numrepetitions):
-        epidemic = False
-        #  countStatuses
-        for stuff in range(0,numrepetitions):
-            s = 0
-            i = 0
-            r = 0
-
-            for item in self.vertices:
-                if item.getStatus() == 'S':
-                   s += 1
-                if item.getStatus() == 'I':
-                    i += 1
-                if item.getStatus() == 'R':
-                    r += 1
-                item.update()
-                #highConcentrationEpidemic
-                if i > .05*len(self.vertices):
-                    epidemic = True
-
-            self.ilist = self.ilist + [i]
-            self.rlist = self.rlist + [r]
-            self.iandrlist = self.iandrlist + [i+r]
-            #print("S is",s,"I is",i,"R is",r)
-
-        newlist = []
-        for item in range(len(self.ilist)-1):
-            newlist = newlist + [self.ilist[item + 1] - self.ilist[item]]
-        newlist2 = []
-        for item in range(len(self.rlist) -1):
-            newlist2 = newlist2 + [self.rlist[item+1] - self.rlist[item]]
-        newlist3 = []
-        for item in range(len(self.iandrlist)-1):
-            newlist3 = newlist3 + [self.iandrlist[item+1] - self.iandrlist[item]]
-        if self.iandrlist[len(self.iandrlist)-1] > .10*len(self.vertices):
+    def countAndUpdateStatuses(self):
+        '''Helper Function for update'''
+        
+        for item in self.vertices:
+            if item.getStatus() == 'S':
+                self.numS += 1
+            if item.getStatus() == 'I':
+                self.numI += 1
+            if item.getStatus() == 'R':
+                self.numR += 1
+            item.update()
             
-            print("epidemic?",'10% at end:', True,",", ".05% I:", epidemic)
-        else:
-            print("epidemic?",'10% at end:', False,",", ".05% I:", epidemic)
+    def updateLists(self):
+        '''Helper function for update'''
+        self.ilist = self.ilist + [self.numI] 
+        self.rlist = self.rlist + [self.numR]
+        self.iandrlist = self.iandrlist + [self.numI+self.numR]
+    def checkHighConcentrationEpi(self):
+        '''Helper Function for update'''
+        if self.numI > .05*len(self.vertices):
+            self.highEpi = True
+    def checkFinalEpi(self):
+        '''Help Function for update '''
+        if self.iandrlist[len(self.iandrlist)-1] > .10*len(self.vertices):
+            self.finalEpi = True
+    def update(self, numrepetitions, numtrials):
+        for stuff in range(0,numrepetitions):
+            self.countAndUpdateStatuses()
+            self.updateLists()
+            self.checkHighConcentrationEpi()
 
-
-
+        self.checkFinalEpi()
+        #print(epidemicfor10/numtrials*100,"%", "epidemic for 10% at end", epidemicfor5/numtrials*100,"%","epidemic for 5% at any time")
+    def getEpis(self):
+        '''Returns boolean values for if epidemics occured. 
+        output: finalEpi, highEpi
+        '''
+        return self.getFinalEpi(), self.getHighEpi()
+    def getFinalEpi(self):
+        return self.finalEpi
+    def getHighEpi(self):
+        return self.highEpi
 
 
 def main():
@@ -81,7 +89,7 @@ def main():
     g = Graph(2, .02, 0, .01)   #k,p,r,%infected
     g.makeVertices(500)         # of people
     g.makeConnections(.02)         #prob they are connected
-    g.update(100)
+    g.update(50,30)                   #number of repetitions, num trials
 
 
 if __name__ == "__main__":
