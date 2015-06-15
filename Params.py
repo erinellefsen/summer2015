77b319d8df0a1ed12c787b02028ee511f03891c9
John Allen Doorenbos <john.doorenbos@gmail.com>
2015-06-15 10:48:28 -0500

    graphs look good. added parameter class to consolidate parameters. about to try and make graph directed

---
+++ Params.py
@@ -0,0 +1,12 @@
+class Params():
+    def __init__(self,k,p,numVert,connectionProb,numVacc):
+        self.k=k
+        self.p=p
+        self.numVerts=numVert
+        self.connectionProb=connectionProb
+        self.percentVacc = float(numVacc)/float(numVert)
+        
+        
+        
+        
+     
