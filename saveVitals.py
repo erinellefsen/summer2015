d8b17b46b74faeaf2fbf799c9e2cd83e7cb6d37d
John Allen Doorenbos <john.doorenbos@gmail.com>
2015-06-29 14:42:33 -0500

    saveVitals should be working. I think

--- saveVitals.py
+++ saveVitals.py
@@ -4,44 +4,77 @@ import tank
 import params as pm
 import minimalGraph as mg
 
-with open('CHANGE_THIS_FILE_NAME.pkl', 'wb') as output:
-    pickler = pickle.Pickler(output,-1)
-    for i in range(4):
-        k=1
-        p=.05
-        vaccination = 0
-        for i in range(15):
+pop = 5000
+vaccLst = [0,.2,.4,.6,.8]
+for ind in range(5):    
+    with open('CHANGE_NAME--RANDOM'+str(ind)+'.pkl', 'wb') as output:
+        pickler = pickle.Pickler(output,-1)
+        for inde in range(4):
+            k=1
+            p=.05
+            for index in range(15):
 
-            j = pm.Params(k,.1,3000,.002,vaccination*3000,targeted= False)
-            g = graph.Graph(j)
-            g.update()
-            r = g.calculateR(True) #basic = true
-            i = g.getR()
-            z = g.getEdges()
-            c = g.getClusteringCoefficient()
-            minG = mg.MinimalGraph(j,r,i,z)
-            tp1 = tank.Tank(minG)
-            pickler.dump(tp1)
+                j = pm.Params(k,.1,pop,.002,vaccLst[inde]*pop,targeted= False)
+                g = graph.Graph(j,random = True)
+                g.update()
+                r = g.calculateR(True) #basic = true
+                i = g.getR()/float(pop)
+                z = g.getEdges()
+                c = g.getClusteringCoefficient()
+                minG = mg.MinimalGraph(j,r,i,c,z,kind="random")
+                tp1 = tank.Tank(minG)
+                pickler.dump(tp1)
+                k += 1
 
+            for index in range(35):
 
+                j = pm.Params(3,p,pop,.002,vaccLst[inde]*pop,targeted = False)
+                g = graph.Graph(j,random=True)
+                g.update()
+                r = g.calculateR(True) #basic = true
+                i = g.getR()/float(pop)
+                z = g.getEdges()
+                c = g.getClusteringCoefficient()
 
+                minG = mg.MinimalGraph(j,r,i,c,z,kind="random")
+                tp1 = tank.Tank(minG)
+                pickler.dump(tp1)
 
-
-            k += 1
-        for i in range(30):
-
-            j = pm.Params(3,p,3000,.002,vaccination*3000,targeted = False)
-            g = graph.Graph(j)
-            g.update()
-            r = g.calculateR(True) #basic = true
-            i = g.getR()
-            z = g.getEdges()
-            c = g.getClusteringCoefficient()
-
-            minG = mg.MinimalGraph(j,r,i,z)
-            tp1 = tank.Tank(minG)
-            pickler.dump(tp1)
-
-            p+= .05
-
+                p+= .05
     output.close()
+    with open('CHANGE_NAME--CLUSTERED'+str(ind)+'.pkl', 'wb') as output1:
+        pickler = pickle.Pickler(output1,-1)
+        for inde in range(4):
+            k=1
+            p=.05
+            vaccination = 0
+            for index in range(15):
+
+                j = pm.Params(k,.1,pop,.002,vaccLst[inde]*pop,targeted= False)
+                g = graph.Graph(j,clustered=True)
+                g.update()
+                r = g.calculateR(True) #basic = true
+                i = g.getR()/float(pop)
+                z = g.getEdges()
+                c = g.getClusteringCoefficient()
+                minG = mg.MinimalGraph(j,r,i,c,z,kind="random")
+                tp1 = tank.Tank(minG)
+                pickler.dump(tp1)
+                k += 1
+
+            for index in range(35):
+
+                j = pm.Params(3,p,pop,.002,vaccLst[inde]*pop,targeted = False)
+                g = graph.Graph(j,clustered=True)
+                g.update()
+                r = g.calculateR(True) #basic = true
+                i = g.getR()/float(pop)
+                z = g.getEdges()
+                c = g.getClusteringCoefficient()
+
+                minG = mg.MinimalGraph(j,r,i,c,z,kind="random")
+                tp1 = tank.Tank(minG)
+                pickler.dump(tp1)
+
+                p+= .05
+    output1.close()
