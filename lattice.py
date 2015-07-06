f1b626cb7bd8404a972a1ac42df21e59fe41d0c4
John Allen Doorenbos <john.doorenbos@gmail.com>
2015-07-06 10:05:15 -0500

    made minor change to savevitals file

--- lattice.py
+++ lattice.py
@@ -5,6 +5,8 @@ class LatticeVertex(Vertex):
         Vertex.__init__(self,key,disease)
         self.x_coord = x
         self.y_coord = y
+        
+
     def getX(self):
         return self.x_coord
     def getY(self):
@@ -12,17 +14,29 @@ class LatticeVertex(Vertex):
     def isNeighbor(self,vert):
         vx = vert.getX()
         vy = vert.getY()
-        isXNeighbor = ((vx+1)==self.x_coord or (vx-1)==self.x_coord) and vy == self.y_coord
+        rightIdX = self.x_coord + 1 
+        leftIdX = self.x_coord - 1
+        isNextToX = vx == leftIdX  or vx == rightIdX
+        isConstantY =  vy == self.y_coord
+        noWrap = not (rightIdX % 3 ==0 or (leftIDX+1)%3 == 0)
+        
+        isXNeighbor = isNextToX and isConstantY and noWrap
         isYNeighbor = ((vy+1)==self.y_coord or (vy-1)==self.y_coord) and vx == self.x_coord
         if (isXNeighbor or isYNeighbor):# and (not(isXNeighbor and isYNeighbor)):
             return True
         return False
-   
+    
+    
+
         
 class LatticeGraph(Graph):
     def __init__(self,params):
         Graph.__init__(self,params)
         self.sideLength = int(math.ceil(math.sqrt(self.numVerts)))
+        self.makeVertices()
+        self.makeConnections()
+        self.vaccinate(self.targeted)
+        
     def makeVertices(self):
         id = 0
         for i in range(self.sideLength):
@@ -38,9 +52,9 @@ class LatticeGraph(Graph):
         for i in range(len(self.vertices)):
             vert1 = self.vertices[i]
             lst = self.getNeighborId(vert1)
-            print "Ns \t",vert1," : ",lst," ", (vert1.x_coord,vert1.y_coord)
+            #print "Ns \t",vert1," : ",lst," ", (vert1.x_coord,vert1.y_coord)
             for i in lst:
-                if i>0 and i < self.numVerts:
+                if i !=None and i>0 and i < self.numVerts:
                     #print vert1.getId()," : ",i
                     self.connect(vert1,self[i],1)
                     self.connect(self[i],vert1,1)
@@ -59,10 +73,25 @@ class LatticeGraph(Graph):
 #                if vert1.isNeighbor(vert2):
 #                    self.connect(vert1,vert2,self.rho)
 #                    self.connect(vert2,vert1,self.rho)
+    def getXNeighbor(self,vert):
+        rightIdX = vert.x_coord + 1 
+        leftIdX = vert.x_coord - 1
+        if rightIdX % self.sideLength ==0 :
+            return [leftIdX]
+        elif (leftIdX+1) % self.sideLength == 0:
+            return [rightIdX]
+        else: 
+            return [rightIdX,leftIdX]
     def getNeighborId(self,vert):
-        n1, n2 = vert.id -1, vert.id+1
-        n3, n4 = vert.id - self.sideLength, vert.id+self.sideLength
-        res = list(set([n1,n2,n3,n4])) 
-        print "res: ",res
+        yu = vert.id - self.sideLength
+        yd = vert.id + self.sideLength
+        if vert.id %self.sideLength ==0:
+            xl = None
+        else:
+            xl = vert.id-1
+        if (vert.id+1) % self.sideLength == 0:
+            xr = None
+        else:
+            xr = vert.id+1
+        res = list(set([xl,xr,yu,yd]))
         return res
-        
--- saveVitals.py
+++ saveVitals.py
@@ -9,7 +9,7 @@ pop = 4000
 vaccLst = [0,.2,.4,.6,.8]
 print("1% Completed")
 for ind in range(5):
-    
+'''    
     with open('CHANGE_NAME--RANDOM'+str(ind)+'.pkl', 'wb') as output:
         pickler = pickle.Pickler(output,-1)
         for inde in range(3):
@@ -49,11 +49,11 @@ for ind in range(5):
 '''
     with open('CHANGE_NAME--CLUSTERED'+str(ind)+'.pkl', 'wb') as output1:
         pickler = pickle.Pickler(output1,-1)
-        for inde in range(4):
+        for inde in range(3):
             k=1
             p=.05
             vaccination = 0
-            for index in range(15):
+            for index in range(13):
                 rho = random.uniform(0.0015,0.004)
                 prams = pm.Params(k,.1,pop,rho,vaccLst[inde]*pop,targeted= False)
                 g = graph.Graph(prams,clustered=True)
@@ -67,7 +67,7 @@ for ind in range(5):
                 pickler.dump(tp1)
                 k += 1
 
-            for index in range(35):
+            for index in range(27):
                 rho = random.uniform(0.0015,0.004)
                 prams = pm.Params(3,p,pop,rho,vaccLst[inde]*pop,targeted = False)
                 g = graph.Graph(prams,clustered=True)
@@ -83,7 +83,8 @@ for ind in range(5):
 
                 p+= .05
     output1.close()
-'''
+    print(str((ind+1)*20)+"% Completed")
+#'''
 
 
 
