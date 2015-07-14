fc9fe67a0968a6a3d1ac942fbd121e137a83af0d
John Allen Doorenbos <john.doorenbos@gmail.com>
2015-07-14 11:40:06 -0500

    Got the r squared calculation workings for the continuous graphs

--- utils.py
+++ utils.py
@@ -2,6 +2,7 @@ from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.patches as mpatches
+from scipy.optimize import brentq
 
 def plot_implicit2d(fn, rangeX=(0,4),rangeY=(0,1),ax=None):
     ''' create a plot of an implicit function
@@ -14,11 +15,13 @@ def plot_implicit2d(fn, rangeX=(0,4),rangeY=(0,1),ax=None):
     B = np.linspace(ymin, ymax, 250) # number of slices
     A1,A2 = np.meshgrid(A,B) # grid on which the contour is plotted
 
-    for z in B: # plot contours in the XY plane
-        X,Y = A1,A2
-        Z = fn(X,Y)
-        cset = ax.contour(X, Y, Z+z, [z], zdir='z')
-        # [z] defines the only level to plot for this contour for this value of z
+    
+    X,Y = A1,A2
+    #print(len(X)," ",len(Y))
+    Z = fn(X,Y)
+    #print(Z+z)
+    cset = ax.contour(X, Y, Z, [0], zdir='z')
+    # [z] defines the only level to plot for this contour for this value of z
 
     # must set plot limits because the contour will likely extend
     # way beyond the displayed level.  Otherwise matplotlib extends the plot limits
@@ -28,15 +31,27 @@ def plot_implicit2d(fn, rangeX=(0,4),rangeY=(0,1),ax=None):
     ax.set_ylim(ymin,ymax)
     return fn.func_name
     
-def continuousInfection(x,y,N=2000,nu=0): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
-    return (np.log((2000-1)/(2000*(1-y)))-x*y)
+    
+def continuousInfection(x=None,y=None,N=2000,nu=0): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
+    if y == None and x != None:
+        
+        g = lambda z: np.log((2000-1-nu)/(2000*(1-z)-nu))-x*z if z != 1 else 1
+        return g
+    elif x == None:
+        g = lambda z: np.log((2000-1-nu)/(2000*(1-z)-nu))-z*y if z != 1 else 1
+        return g
+    else:
+        return np.log((2000-1-nu)/(2000*(1-y)-nu))-x*y
 
-def continuousInfection2(x,y,N=2000,nu=400): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
-    return (np.log((2000-1-400)/(2000*(1-y)-400))-x*y)
-def continuousInfection4(x,y,N=2000,nu=800): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
-    return (np.log((2000-1-800)/(2000*(1-y)-800))-x*y)
-def continuousInfection6(x,y,N=2000,nu=1200): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
-    return (np.log((2000-1-1200)/(2000*(1-y)-1200))-x*y)
+def continuousInfection2(x,y=None,N=2000,nu=400): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
+    return (np.log((2000-1-nu)/(2000*(1-y)-nu))-x*y)
+
+def continuousInfection4(x,y=None,N=2000,nu=800): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
+    return (np.log((2000-1-nu)/(2000*(1-y)-nu))-x*y)
+
+def continuousInfection6(x,y=None,N=2000,nu=1200): #CHECK THIS OUT AND DON'T FORGET THAT SOME ARE VACCINATED
+    return (np.log((2000-1-nu)/(2000*(1-y)-nu))-x*y)
+
 
 def plot_implicit3d(fn, bbox=(0,5)):
     ''' create a plot of an implicit function
@@ -88,3 +103,44 @@ def plot_implicit3d(fn, bbox=(0,5)):
 
 def eq( a, b, eps=0.000001 ):
     return abs(a - b) <= eps
+
+
+def calcRSquared(f,popt,xLst,yLst): #input: function, xlist and y list 
+    SSreg = calcSSreg(f,popt,xLst,yLst)
+    SStot = calcSStot(yLst) #mean
+    print(SSreg,SStot)
+    return 1- (SSreg/SStot)
+
+def calcSSreg(f,popt,xLst,yLst):
+    res = 0
+    for i in range(len(xLst)):
+        x = xLst[i]
+        yGuess = yLst[i]
+        temp = yGuess - f(x,*popt) 
+        res += temp**2
+    return res
+def calcSStot(yLst):
+    res = 0
+    mean = np.mean(yLst)
+    for i in yLst:
+        res += (mean-i)**2
+    return res
+
+
+def calcRSquaredImplicit(f,xLst,yLst):
+    
+    SSreg = calcSSregImplicit(f,xLst,yLst) #8.6
+    SStot = calcSStot(yLst) #3.5
+    print(SSreg,SStot)
+    return 1- (SSreg/SStot)
+        
+def calcSSregImplicit(f,xLst,yLst):
+    res = 0
+    for i in range(len(xLst)):
+        x = xLst[i]
+        yGuess = yLst[i]
+        g = f(x=x) #return a single variable function
+        yReal = brentq(g,0,1)
+        temp = yReal-yGuess
+        res += temp**2
+    return res
